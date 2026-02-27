1ccdfe0a0ff349325b382a0d53bea88ef5a4c821
kuiningzzzz <1396298976@qq.com>
2026-02-27 21:14:48 +0800

    (fix)小程序端删掉自动更新机制

--- App.vue
+++ App.vue
@@ -12,58 +12,8 @@ export default {
     console.log('App Hide')
   },
   methods: {
-    async initApp() {
+    initApp() {
       // 不在这里做登录检查，让各个页面自己处理
-      await this.checkForUpdates()
-    },
-    async checkForUpdates() {
-      // #ifdef APP-PLUS
-      try {
-        console.log('检查应用版本更新...')
-        const baseInfo = uni.getAppBaseInfo ? uni.getAppBaseInfo() : {}
-        const versionCode = Number(baseInfo.appVersionCode || 0)
-
-        const api = require('./utils/api.js').default
-        const res = await api.checkAppVersion(versionCode)
-
-        if (!res.isLatest && res.latestVersion) {
-          uni.setStorageSync('pendingUpdate', res.latestVersion)
-          uni.reLaunch({
-            url: '/pages/update/update'
-          })
-        }
-      } catch (error) {
-        console.error('版本检查失败:', error)
-      }
-      // #endif
-      
-      // #ifdef MP-WEIXIN
-      // 小程序使用微信内置的更新机制
-      try {
-        const updateManager = uni.getUpdateManager()
-        updateManager.onCheckForUpdate((res) => {
-          if (res.hasUpdate) {
-            console.log('小程序有新版本')
-          }
-        })
-        updateManager.onUpdateReady(() => {
-          uni.showModal({
-            title: '更新提示',
-            content: '新版本已经准备好，是否重启应用？',
-            success: (res) => {
-              if (res.confirm) {
-                updateManager.applyUpdate()
-              }
-            }
-          })
-        })
-        updateManager.onUpdateFailed(() => {
-          console.log('新版本下载失败')
-        })
-      } catch (error) {
-        console.error('小程序版本检查失败:', error)
-      }
-      // #endif
     }
   }
 }
